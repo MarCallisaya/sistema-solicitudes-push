fe201b100fc72d402e8ae237d104cc66850219f3
MarCallisaya <vmarcallisaya@gmail.com>
2026-04-08 00:59:49 -0400

    8-4-26-en config.php Corregir sesiones para Render

--- application/config/config.php
+++ application/config/config.php
@@ -393,6 +393,8 @@ $config['encryption_key'] = '';
 | except for 'cookie_prefix' and 'cookie_httponly', which are ignored here.
 |
 */
+
+/*
 $config['sess_driver'] = 'files';
 $config['sess_cookie_name'] = 'ci_session';
 $config['sess_samesite'] = 'Lax';
@@ -401,7 +403,19 @@ $config['sess_save_path'] = NULL;
 $config['sess_match_ip'] = FALSE;
 $config['sess_time_to_update'] = 300;
 $config['sess_regenerate_destroy'] = FALSE;
+*/
 
+
+// 8/4/26 configuracion para despliegue a la nube
+$config['sess_driver'] = 'files';
+$config['sess_cookie_name'] = 'ci_session';
+$config['sess_samesite'] = 'Lax';
+$config['sess_expiration'] = 7200;
+$config['sess_save_path'] = sys_get_temp_dir();
+$config['sess_match_ip'] = FALSE;
+$config['sess_time_to_update'] = 300;
+$config['sess_regenerate_destroy'] = FALSE;
+$config['cookie_secure'] = TRUE;
 /*
 |--------------------------------------------------------------------------
 | Cookie Related Variables
